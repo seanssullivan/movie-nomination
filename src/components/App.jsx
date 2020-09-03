a3d981ec74d9a155147830a178518c1987e2a175
Sean Sullivan <dev@seanmedia.ca>
2020-09-02 18:07:44 -0600

    Reorganize project directory

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <main></main>
+    </div>
+  );
+}
+
+export default App;
